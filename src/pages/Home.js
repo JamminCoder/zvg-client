6fd9496f1d7928df386e43642ea1e0de60d8b809
Timothy Batt <jammincoderguy96@gmail.com>
2022-08-25 12:01:10 -0400

    Basic slider working on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,81 @@
 import "../css/Home.css";
 
 
+export function Slide({ img, header, lead, buttons, sliderNum, max }) {
+
+  const indicators = [];
+  for (let i = 1; i <= max; i++) {
+    let style = {backgroundColor: "grey"}
+    
+    if (i === sliderNum) style = {backgroundColor: "white"};
+
+
+    let indicator = <span key={i} class="w-2 aspect-square bg-white inline-block mx-2 rounded-full" style={style}></span>
+    indicators.push(indicator);
+  }
+
+  return (
+    <section className="slide flex-grow w-[100vw] relative grid place-items-center">
+
+      <div class="w-[100vw]">
+        {/* Image */}
+        <img src={ img } class="h-[50vh] md:h-[70vh] w-[100%] object-cover" />
+      </div>
+
+      <div class="welcome-header-section w-[100%] h-[100%] absolute grid place-items-center">
+        <div class="flex flex-col items-center gap-2">
+
+          {/* Header and lead */}
+          <h1 class="text-white text-center text-3xl md:text-7xl font-bold ">{ header }</h1>
+          <p class="text-center text-white text-2xl font-light max-w-[30rem]">{ lead }</p>
+          
+          <div class="flex justify-center gap-2 mt-2">
+
+            {/* Buttons */}
+            { buttons }
+          
+          </div>
+
+          {/* Slider indicators */}
+          <div class="mt-5">
+           { indicators }
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+}
+
+
 export function HeaderSection(props) {
   return (
-    <div>
-      <section class="relative grid place-items-center">
-        <div class="w-[100%]">
-          <img src='/img/zoar_valley.jpg' class="h-[50vh] md:h-[70vh] w-[100%] object-cover" />
-        </div>
-        
-        <div class="welcome-header-section w-[100%] h-[100%] absolute grid place-items-center">
-          <div class="flex flex-col items-center gap-2">
+    <div className="welcome-slider">
+        <Slide 
+          img='/img/zoar_valley.jpg'
+          header="Zoar Valley Gifts & More Store"
+          lead="lorem ipsum something awesome, I wish I could come up with cool things to say"
+          buttons={[
+            <a class="px-2 py-1 bg-white bg-opacity-75 rounded text-xl">Shop</a>,
+            <a class="px-2 py-1 bg-white bg-opacity-75 rounded text-xl">Campgrounds</a>
+          ]}
 
-            <h1 class="text-white text-center text-3xl md:text-7xl font-bold ">Zoar Valley Gifts & More</h1>
-            <p class="text-center text-white text-2xl font-light max-w-[30rem]">lorem ipsum something awesome, I wish I could come up with cool things to say</p>
-            
-            <div class="flex justify-center gap-2 mt-2">
-              <a class="px-2 py-1 bg-white bg-opacity-75 rounded text-xl">Shop</a>
-              <a class="px-2 py-1 bg-white bg-opacity-75 rounded text-xl">Campgrounds</a>
-            </div>
+          sliderNum={1}
+          max={2}
+        />
 
-          </div>
-        </div>
-      </section>
+        <Slide 
+          img='/img/zoar_valley.jpg'
+          header="The Second Slide"
+          lead="Backend development is better"
+          buttons={[
+            <a class="px-2 py-1 bg-white bg-opacity-75 rounded text-xl">Shop</a>,
+            <a class="px-2 py-1 bg-white bg-opacity-75 rounded text-xl">Campgrounds</a>
+          ]}
 
+          sliderNum={2}
+          max={2}
+        />
     </div>
   );
 }
