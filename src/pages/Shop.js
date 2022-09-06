685f4a4b490faabbf47edff078c7058b40e22735
Timothy Batt <jammincoderguy96@gmail.com>
2022-09-06 13:14:41 -0400

    Removed padding on shop page and change header font size

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -55,16 +55,16 @@ function Catagories(props) {
 
 export default function Shop(props) {
     return (
-        <div className="flex flex-col items-center px-5">
+        <div className="flex flex-col items-center">
             <div className="w-[100%] max-w-[110rem]">
                 <HeroSection 
                     className="grid place-items-center max-h-[65vh] w-[100%] aspect-video"
                     bgAlt="filler pic"
                     bgSrc=""
                 >
-                    <div className="text-center">
-                        <h1 className="text-6xl mb-5">The Shop</h1>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam obcaecati eius explicabo repudiandae repellendus quisquam tempore.</p>
+                    <div className="text-center grid place-items-center">
+                        <h1 className="text-4xl sm:text-6xl mb-5">The Shop</h1>
+                        <p className="max-w-[80%]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam obcaecati eius explicabo repudiandae repellendus quisquam tempore.</p>
                     </div>
                 </HeroSection>
                 
