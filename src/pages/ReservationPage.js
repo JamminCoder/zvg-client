4159edd5016039a30758fb8a8d4cf1c222a88ae4
Timothy Batt <jammincoderguy96@gmail.com>
2022-09-08 11:31:00 -0400

    Started work on campground section

--- src/pages/ReservationPage.js
+++ src/pages/ReservationPage.js
@@ -4,8 +4,20 @@ export default function ReservationPage() {
     return (
         <div>
             <HeroSection bgSrc={`${process.env.PUBLIC_URL}/img/cabin.png`}
-            className="grid place-items-center max-h-[65vh] w-[100%] aspect-video">
-                <h1 className="text-white text-4xl">Hello</h1>
+            className="grid place-items-center h-[90vh] w-[100%]">
+                <div className="text-white text-center">
+                    <h1 className="text-6xl">Campgrounds &amp; Cabins</h1>
+                    <p className="mt-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit qui ex quisquam expedita eligendi veniam, pariatur eius Quis harum porro animi molestias.
+                    </p>
+
+                    <div className="mt-8 flex gap-3 justify-center">
+                        <a href="#reserve" className="py-2 px-4 text-white text-xl bg-blue-600 rounded">Reserve</a>
+                        <a href="#reserve" className="py-2 px-4 text-black text-xl bg-slate-200 rounded">Something</a>
+                        <a href="#reserve" className="py-2 px-4 text-black text-xl bg-orange-400 rounded">Awesome</a>
+                    </div>
+                </div>
+                
             </HeroSection>
             <main>
                 Hello!
