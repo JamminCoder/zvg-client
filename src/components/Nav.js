b1aee154f29c93add9f7a1c83a706f3a98abd5c5
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-03 17:20:07 -0400

    Fixed `fill-rule` warning

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -71,7 +71,7 @@ export default function Nav() {
                 <svg 
                     fill="currentColor" viewBox="0 0 16 16" id="nav-btn"
                     className="h-10 w-10 hover:bg-slate-200 active:bg-slate-300 transition-colors rounded cursor-pointer" onClick={ handleCollapse } xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>    
+                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>    
                 </svg>
 
                 <span className="cart-notification">{ itemCount == 0 ? "": itemCount }</span>
