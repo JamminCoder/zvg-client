4bcd2a8b7420898650ce4eb88d9a97ce8986693c
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-06 13:52:53 -0400

    Log in is functional

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,4 @@
 export const SERVER_URL = "http://localhost:8000"
 
-export const API_PRODUCT_NEW = `${ SERVER_URL }/products/new`
+export const API_PRODUCT_NEW = `${ SERVER_URL }/products/new`;
+export const API_LOGIN = `${ SERVER_URL }/login`;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,29 +2,29 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Form from '../components/Form';
 import CenterPage from '../components/CenterPage';
+import { API_LOGIN } from "../apiConfig";
 const axios = require('axios').default;
 
 
 export default function Login(props) {
     const [message, setMessage] = useState({ color: null, text: "" });
-    const action = "http://localhost:8000/login";
 
     function onSubmit(e) {
         e.preventDefault();
 
         const password = document.querySelector("#password").value;
-        const username = document.querySelector("#username").value;
+        const email = document.querySelector("#email").value;
         
         // Submit POST request to login-endpoint. 
         // On success, store JWT in localStorage
         // On fail display error message
-        axios.post(action, {username: username, password: password})
+        axios.post(API_LOGIN, {email: email, password: password})
             .then(res => {
             if (res.data.jwt) {
                 // Server returned a JWT, that means the login was successful.
                 setMessage({ color: "green", text: "Success!" });
                 localStorage.setItem("jwt", res.data.jwt);
-                localStorage.setItem("username", username);
+                localStorage.setItem("email", email);
 
             } else {
                 // No JWT in response, display the response error message.
@@ -36,15 +36,15 @@ export default function Login(props) {
 
     return (
         <CenterPage>
-            <Form id="loginForm" method="POST" action={ action } onSubmit={ (e) => onSubmit(e) }>
+            <Form id="loginForm" method="POST" action={ API_LOGIN } onSubmit={ (e) => onSubmit(e) }>
                 <h1 className="text-3xl">Login</h1>
 
                 <p className='text-center mt-2 h-3' style={{ color: message.color }}>{ message.text }</p>
 
                 <div className="my-5">
                     <div className="mb-4">
-                        <label htmlFor="username">Username</label><br/>
-                        <input className="border" type="text" name="username" id="username"/>
+                        <label htmlFor="email">Email</label><br/>
+                        <input className="border" type="email" name="email" id="email"/>
                     </div>
 
                     <div className="mb-4">
