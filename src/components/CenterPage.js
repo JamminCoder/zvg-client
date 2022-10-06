92a1b34177d4b3fadd22dc1bd17ea289482b64be
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-05 20:37:27 -0400

    `<CenterPage>` now uses `place-content-center` instead of `place-items-center`

--- src/components/CenterPage.js
+++ src/components/CenterPage.js
@@ -1,6 +1,6 @@
 export default function CenterPage(props) {
     return (
-        <div className='grid place-items-center h-[90vh] px-2'>
+        <div className='grid place-content-center h-[90vh] px-2'>
             { props.children }
         </div>
     );
