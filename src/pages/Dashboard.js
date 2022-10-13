156fd9d620860aace3fb627d35bb07550d9c6068
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-13 13:24:44 -0400

    Added form fields to product creation model

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,31 +4,38 @@ import { isVerified, isLoggedIn } from "../auth";
 import GridEvenContainer from "../components/GridEvenContainer";
 import { Sidebar } from "../components/Sidebar";
 import { API_PRODUCT_NEW } from "../apiConfig";
-import { preventDefaults } from "../utils";
+import { getCookie, preventDefaults } from "../utils";
 import axios from "axios";
 
 
 export function NewItemModal(props) {
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
 
     function submit(e) {
         preventDefaults(e);
 
-        const images = document.querySelector("#product_images").files;
-        const productName = document.querySelector("#product_name").value;
-        const productDescription = document.querySelector("#product_description").value;
-        const productPrice = document.querySelector("#product_price").value;
+        const formData = new FormData(document.querySelector("#new_product_form"));
+        
+        const requestOptions = { 
+            headers: { "X-XSRF-TOKEN": getCookie("XSRF-TOKEN") }, 
+            withCredentials: true 
+        };
 
-        for (let key in images) {
-            console.log(images[key]);
-        }
+        axios.post(API_PRODUCT_NEW, formData, requestOptions).then(res => {
+            if (res.status === 200) {
+                setSuccess("Successfully created new product");
+            }
 
-        console.log(productName);
-        console.log(productDescription);
-        console.log(productPrice);
+        }).catch(err => {
+            const errors = err.response.data.errors;
+            
+            for (let error in errors) {
+                setError(errors[error]);
+                break;
+            }
 
-        const form = new FormData(document.querySelector("#new_product_form"));
-        axios.post(API_PRODUCT_NEW, form).then(res => {
-            console.log(res);
+            console.log(errors);
         });
 
     }
@@ -37,32 +44,49 @@ export function NewItemModal(props) {
         <div className="fixed z-50">
             <div className="fixed bg-black opacity-25 w-full h-full"></div>
             <div className="fixed grid place-content-center w-full h-full">
+                
+                {/* Main model */}
                 <div className="shadow bg-white p-8 rounded">
                     <h1 className="text-black font-medium text-3xl">Upload new product</h1>
+                    
+                    <p className="text-green-600"> { success } </p>
+                    <p className="text-red-600"> { error } </p>
+
                     <form id="new_product_form" action={ API_PRODUCT_NEW } method="POST" encType="multipart/form-data" className="py-2 flex flex-col gap-4" onSubmit={ submit }>
                         <div>
                             <label htmlFor="images" className="text-lg">Product Images</label><br/>
-                            <input type="file" id="product_images" name="product_images" multiple/>
+                            <input type="file" id="images" name="images" multiple required/>
                         </div>
 
                         <div>
-                            <label htmlFor="product_name" className="text-lg">Product Name</label><br/>
-                            <input type="text" id="product_name" name="product_name" className="border"/>
+                            <label htmlFor="catagory" className="text-lg">Catagory</label><br/>
+                            <input type="text" id="catagory" name="catagory" className="border" required/>
                         </div>
 
                         <div>
-                            <label htmlFor="product_description" className="text-lg">Description</label><br/>
-                            <input type="text" id="product_description" name="product_description" className="border"/>
+                            <label htmlFor="name" className="text-lg">Product Name</label><br/>
+                            <input type="text" id="name" name="name" className="border" required/>
                         </div>
 
                         <div>
-                            <label htmlFor="product_price" className="text-lg">Price</label><br/>
-                            $<input type="number" id="product_price" name="product_price" className="max-w-fit border"/>
+                            <label htmlFor="sku" className="text-lg">SKU (Stock Keeping Unit)</label><br/>
+                            <input type="text" id="sku" name="sku" className="border" required/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="description" className="text-lg">Description</label><br/>
+                            <input type="text" id="description" name="description" className="border"/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="price" className="text-lg">Price</label><br/>
+                            $<input type="number" id="price" name="price" className="max-w-fit border" required/>
                         </div>
 
                         <button className="border px-2 py-1 w-fit rounded hover:bg-slate-50 active:bg-slate-100">Create</button>
                     </form>
                 </div>
+
             </div>
         </div>
     );
