9aeb0684563ae99d30ee5f661de01c72eea3246b
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-14 13:01:54 -0400

    Removed unused component

--- src/components/Form.js
+++
@@ -1,7 +0,0 @@
-export default function Register(props) {
-    return (
-        <form onSubmit={ props.onSubmit } action={ props.action } method={ props.method } className="flex flex-col items-center bg-white border border-gray-400 rounded-lg w-[100%] max-w-[30em] py-10 px-2">
-            { props.children }
-        </form>
-    );
-}
