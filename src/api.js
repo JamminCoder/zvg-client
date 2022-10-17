24eb1e281b1d71dd9881a882cedded79c0f6c12a
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-17 11:52:29 -0400

    Removed unused `XSRF_HEADER` from api call

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import { XSRF_HEADER } from "./lib/auth";
 const axios = require("axios").default;
 
 export async function getAllProducts() {
-    const res = await axios.get(API_PRODUCTS_ALL, XSRF_HEADER);
+    const res = await axios.get(API_PRODUCTS_ALL);
     const products = res.data;
     console.log(products);
     return products;
