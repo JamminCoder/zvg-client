5dea13f389628d5715a3dd88b40d7fe3eccf51dc
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-28 11:00:22 -0400

    implementing paypal api from scratch

--- src/components/PaypalCheckout.js
+++ src/components/PaypalCheckout.js
@@ -7,19 +7,14 @@ export function CardIntegration() {
         <div className="card_container">
             <form id="card-form">
                 <label htmlFor="card-number">Card Number</label>
-                <div id="card-number" className="card_field"></div>
+                <input id="card-number" name="card-number" className="card_field"/>
 
                 <div>
                     <label htmlFor="expiration-date">Expiration Date</label>
-                    <div id="expiration-date" className="card_field"></div>
+                    <input id="expiration-date" name="expiration-date" className="card_field"/>
                 </div>
 
-                <div>
-                    <label htmlFor="cvv">CVV</label><div id="cvv" className="card_field"></div>
-                    </div>
-                    <label htmlFor="card-holder-name">Name on Card</label>
-                    <input type="text" id="card-holder-name" name="card-holder-name" autoComplete="off" placeholder="card holder name"/>
-                
+ 
                 <div>
                     <label htmlFor="card-billing-address-street">Billing Address</label>
                     <input type="text" id="card-billing-address-street" name="card-billing-address-street" autoComplete="off" placeholder="street address"/>
@@ -32,18 +27,20 @@ export function CardIntegration() {
                 
                 <div>
                     <input type="text" id="card-billing-address-city" name="card-billing-address-city" autoComplete="off" placeholder="city"/>
-                    </div>
+                </div>
+
                 <div>
                     <input type="text" id="card-billing-address-state" name="card-billing-address-state" autoComplete="off" placeholder="state"/>
-                    </div>
+                </div>
+
                 <div>
                     <input type="text" id="card-billing-address-zip" name="card-billing-address-zip" autoComplete="off" placeholder="zip / postal code"/>
-                    </div>
+                </div>
                 
                 <div>
                     <input type="text" id="card-billing-address-country" name="card-billing-address-country" autoComplete="off" placeholder="country code" />
-                </div>
-                <br/><br/>
+                </div><br/>
+
                 <button value="submit" id="submit" className="btn">Pay</button>
             </form>
         </div>
@@ -87,20 +84,11 @@ export default function PaypalCheckout(props) {
 
     return (
         <div className="block">
-            <link rel="stylesheet" type="text/css" href="https://www.paypalobjects.com/webstatic/en_US/developer/docs/css/cardfields.css"/>
-
-            <script
-                src={`https://www.paypal.com/sdk/js?components=buttons,hosted-fields&client-id=${ clientID }`}
-                data-client-token={ clientToken }
-            ></script>
-
             <PaypalButton/>
             
             <p className="my-5 mx-auto">or pay with credit card</p>
 
             <CardIntegration/>
-
-            <script src="/checkout.js"></script>
         </div>
     );
 }
