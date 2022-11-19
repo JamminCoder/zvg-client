1877a91ebb62b8854d2549d7b8b268c8aabb34f0
Timothy Batt <jammincoderguy96@gmail.com>
2022-11-18 20:16:14 -0500

    added edit catagory button

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -52,16 +52,26 @@ export function ProductsList({ catagory }) {
     <div>
         { modal }
         <h2 className="text-2xl mb-5">{ catagory.name }</h2>
+        
+        <div className="flex gap-12 mb-8">
+            <Button
+                className="text-white bg-gray-800 text-xs rounded" 
+                onClick={ () => console.log("edit") }>
+                
+                Edit { catagory.name } Catagory
+            </Button>
+
+            <Button 
+                className="text-white bg-red-500 text-xs rounded" 
+                onClick={ handleConfirmDelete }>
+                
+                Delete { catagory.name } Catagory
+            </Button>
+        </div>
+
         <div className="flex flex-wrap gap-8 mb-4">
             { catagory.products.map(product => <AdminProductCard key={ product.sku } product={ product } />)}
         </div>
-
-        <button 
-            className="p-2 text-white bg-red-500 text-xs rounded hover:brightness-105 active:brightness-90" 
-            onClick={ handleConfirmDelete }>
-            
-            Delete { catagory.name } Catagory
-        </button>
     </div>
     );
 }
