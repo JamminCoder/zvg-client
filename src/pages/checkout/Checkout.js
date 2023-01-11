121a68703986e886b40f1f7af8fdac64337267bf
Timothy Batt <jammincoderguy96@gmail.com>
2023-01-11 12:20:17 -0500

    remove checkout button if cart is empty

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -2,6 +2,7 @@ import ProductList from "../../components/ProductList";
 import { useEffect, useState } from "react";
 import ShoppingCartManager from "../../lib/shoppingCartManager";
 import SquareCheckout from "./SquareCheckout";
+import { Link } from "react-router-dom";
 
 
 
@@ -21,15 +22,20 @@ export default function Checkout(props) {
 
     return (
         <div className="px-4 py-24 mx-auto flex justify-center gap-12">
+            
             <div className="bg-white rounded p-2 z-20 shadow-lg max-w-[50rem] flex-grow text-xl">
-                
+                <h1 className="text-4xl font-medium mb-8">Checkout</h1>    
                 <ProductList className="mb-5" cartItems={ cartItems }/>
 
                 Tax: ${ tax }
                 <div className="text-xl mb-8">
                     Total: ${ price }
                 </div>
-                <SquareCheckout/>
+                { 
+                    cartItems.length
+                    ? <SquareCheckout/>
+                    : <h2 className="text-2xl">No items in cart. Go get some in the <Link className="link" to="/shop">Shop!</Link></h2>
+                }
             </div>   
         </div>
     );
